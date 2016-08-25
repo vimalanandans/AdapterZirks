c6b005bde5ce19b732eddaa38a8f8faa8775b44f
Michael Maass <mam1pi@bosch.com>
2016-08-25 15:36:56 -0400

    Initial commit of estimote beacon attributes

---
+++ EstimoteAdapter/estimoteadapter/src/main/java/com/bezirk/adapterzirks/estimote/EstimoteBeaconAttributesEvent.java
@@ -0,0 +1,117 @@
+package com.bezirk.adapterzirks.estimote;
+
+import com.bezirk.middleware.messages.Event;
+
+public class EstimoteBeaconAttributesEvent extends Event {
+    private final String bootloaderVersion;
+    private final long currentMotionStateDuration;
+    private final String firmwareVersion;
+    private final String hardwareVersion;
+    private final String identifier;
+    private final boolean isMoving;
+    private final long lastMotionStateDuration;
+    private final EstimoteOrientation orientation;
+    private final int rssi;
+    private final double xAcceleration;
+    private final double yAcceleration;
+    private final double zAcceleration;
+
+    public EstimoteBeaconAttributesEvent(String bootloaderVersion, long currentMotionStateDuration,
+                                         String firmwareVersion, String hardwareVersion,
+                                         String identifier, boolean isMoving,
+                                         long lastMotionStateDuration, EstimoteOrientation orientation,
+                                         int rssi, double xAcceleration,
+                                         double yAcceleration, double zAcceleration) {
+        this.bootloaderVersion = bootloaderVersion;
+        this.currentMotionStateDuration = currentMotionStateDuration;
+        this.firmwareVersion = firmwareVersion;
+        this.hardwareVersion = hardwareVersion;
+        this.identifier = identifier;
+        this.isMoving = isMoving;
+        this.lastMotionStateDuration = lastMotionStateDuration;
+        this.orientation = orientation;
+        this.rssi = rssi;
+        this.xAcceleration = xAcceleration;
+        this.yAcceleration = yAcceleration;
+        this.zAcceleration = zAcceleration;
+    }
+
+    public String getBootloaderVersion() {
+        return bootloaderVersion;
+    }
+
+    public long getCurrentMotionStateDuration() {
+        return currentMotionStateDuration;
+    }
+
+    public String getFirmwareVersion() {
+        return firmwareVersion;
+    }
+
+    public String getHardwareVersion() {
+        return hardwareVersion;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public long getLastMotionStateDuration() {
+        return lastMotionStateDuration;
+    }
+
+    public EstimoteOrientation getOrientation() {
+        return orientation;
+    }
+
+    public int getRssi() {
+        return rssi;
+    }
+
+    public double getXAcceleration() {
+        return xAcceleration;
+    }
+
+    public double getYAcceleration() {
+        return yAcceleration;
+    }
+
+    public double getZAcceleration() {
+        return zAcceleration;
+    }
+
+    public enum EstimoteOrientation {
+        HORIZONTAL, HORIZONTAL_UPSIDE_DOWN, LEFT_SIDE, RIGHT_SIDE, UNKNOWN, VERTICAL,
+        VERTICAL_UPSIDE_DOWN
+    }
+
+    public enum EstimoteType {
+        ALL("all"),
+        BAG("bag"),
+        BED("bed"),
+        BIKE("bike"),
+        CAR("car"),
+        CHAIR("chair"),
+        DOG("dog"),
+        DOOR("door"),
+        FRIDGE("fridge"),
+        GENERIC("generic"),
+        SHOE("shoe"),
+        UNKNOWN("unknown");
+
+        private final String text;
+
+        private EstimoteType(final String text) {
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+}
