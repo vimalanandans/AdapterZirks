255b600dba573450195bd7294690fd1f2d3ae66e
unknown <DEV6KOR@KOR1056332.KOR.APAC.BOSCH.COM>
2016-09-13 20:30:07 +0530

    Added Testcases

---
+++ OBDAdapter/obdadapterzirk/src/main/java/com/bezirk/adapter/obd/events/RequestObdErrorCodesEvent.java
@@ -0,0 +1,22 @@
+package com.bezirk.adapter.obd.events;
+
+import com.bezirk.middleware.messages.Event;
+
+/**
+ * Created by DEV6KOR on 9/8/2016.
+ */
+public class RequestObdErrorCodesEvent extends Event
+{
+    private String attribute;
+
+    public static final String TOPIC = RequestObdErrorCodesEvent.class.getSimpleName();
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute)
+    {
+        this.attribute = attribute;
+    }
+}
